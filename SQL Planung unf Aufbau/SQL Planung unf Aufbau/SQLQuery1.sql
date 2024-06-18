286cd5e9c7e1ba8b695def9b612e47427d324a33
Rauchendraser <andreasr@ppedv.de>
2024-06-18 16:58:38 +0200

    Agent DB Objekte

---
+++ SQL Planung unf Aufbau/SQL Planung unf Aufbau/SQLQuery1.sql
@@ -0,0 +1,79 @@
+--Recap Tag 1
+
+
+--Setup:
+
+--Was kann man nach dem Setup nicht mehr ändern?
+
+/*
+MAXDOP  max Anzahl an Kernen pro Abfrage
+max Kerne: 8
+
+tempdb 
+Anzahl der Dateien = Anzahl der Kerne  max 8
+gleiche Größe
+
+RAM
+max RAM : Gesamter RAM - OS
+
+Security
+NT Login
+gemischte Auth (SA: komplexes Kennwort)
+
+Standardpfade für Log und Datendateien
+.ldf  - .mdf
+bei Änderung Neustart!
+
+
+Filestreaming
+Konfigurationsmanager
+
+nich veränderbar
+Binärkram
+= fix im Programme Verzeichnis
+
+
+
+
+SystemDBs
+
+model: Vorlage
+msdb: für den Agent (Jobs, Zeitpläne,...,Email)
+master: Infos zu DBs , Logins
+tempdb: Müllschlucker
+
+
+!! IO reduzieren  !!!
+
+DB Setting
+--Startgrößen anpassen ( in 3 Jahren ??)
+--Wachstumsrate +64 MB --> 1024
+
+Volumewartungstask
+Lok Sicherheitsrichtlinie--zuweisen von Benutzerrechten-- Durchführen von Volumew...
+Dienstkonto reinsetzen
+
+
+
+
+
+
+
+
+
+
+
+
+
+schulung\administrator
+ppedv2023!
+
+
+
+
+
+
+
+
+*/
+
